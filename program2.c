9eb8453f79daa37bf0281c1a6ff707f74f4e6722
Devieswaran <devi.eswaran98@gmail.com>
2017-07-10 12:29:02 +0530

    Create program2.c

---
+++ program2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char g;
+printf("\n Enter the word");
+scanf("%s",&g);
+if(g=a // g=e //g=i //g=o//g=u)
+{
+printf("\n vowels");
+}
+else
+{
+printf("\n consonants");
+}
+return o;
+}
